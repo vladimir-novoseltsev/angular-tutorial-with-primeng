df6dfe1830afd68e04599c53792416ad0fd2454b
Vladimir Novoseltsev <vn@delai-delo.by>
2018-02-05 13:12:25 +0300

    Integrating primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
@@ -22,6 +23,7 @@ import { MessageService } from './message.service';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     AppRoutingModule,
   ],
